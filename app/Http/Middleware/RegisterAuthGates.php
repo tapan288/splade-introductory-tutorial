b4eccd19319814989c5451b5f1ff3e394a49ffb2
tapan288 <tapansharma69@gmail.com>
2023-03-07 23:10:25 +0545

    register auth gates using middleware

---
+++ app/Http/Middleware/RegisterAuthGates.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegisterAuthGates
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user()->loadMissing('roles.permissions');
+
+        if ($user) {
+            foreach ($user->roles as $role) {
+                foreach ($role->permissions as $singlePermission) {
+                    $permissions[] = $singlePermission->title;
+                }
+            }
+
+            collect($permissions)->unique()->each(function ($permission) {
+                Gate::define($permission, function ($user) {
+                    return true;
+                });
+            });
+        }
+
+        return $next($request);
+    }
+}
